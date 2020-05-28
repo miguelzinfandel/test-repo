3b4629942dae286a3f65d82826e8b4aefe85cdb5
miguelzinfandel <miguel.zinfandel@gmail.com>
2020-05-28 10:32:52 +0100

    Removed some code

--- hello.py
+++ hello.py
@@ -1,23 +1,8 @@
 #
 # A simple python Script
 #
-import math
-import sys
-from os import rename
-
 import requests
-
-print("Sys:", sys.version)
-print("Exe:", sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-
-name = input("Your name? ")
-print("Hello, ", name)
+print(r.ok)
